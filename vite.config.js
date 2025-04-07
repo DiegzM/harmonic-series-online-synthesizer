d074c5b806356e5c3f6af3cd6b14a66fd3007282
DiegzM <147036619+DiegzM@users.noreply.github.com>
2025-04-07 12:45:43 -0700

    Fix vite config base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig(({command}) => ({
 
-  base: command === 'build' ? '/harmonic-series-online-synthesizer/' : '/',
+  base: '/',
 
   server: {
     https: {
