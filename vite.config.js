8b24b5b78d8757c23490486fa604523415658767
DiegzM <147036619+DiegzM@users.noreply.github.com>
2025-04-07 10:00:28 -0700

    fix node modules

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,18 @@ export default defineConfig({
       port: 5173, // Ensure this matches your Vite server port
     },
   },
+  resolve: {
+    alias: {
+      'midi-file': path.resolve(__dirname, './node_modules/midi-file'),
+      '@tonejs/midi': path.resolve(__dirname, './node_modules/@tonejs/midi')
+    }
+  },
+  optimizeDeps: {
+    include: ['midi-file', '@tonejs/midi']
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  }
 });
